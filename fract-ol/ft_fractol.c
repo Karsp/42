e3fc7184aa79f7a755de096ceaa85a601d16980c
David Aviles Leiva <daviles-@c1r11s2.42madrid.com>
2023-06-02 15:26:29 +0200

    Working on

--- fract-ol/ft_fractol.c
+++ fract-ol/ft_fractol.c
@@ -6,7 +6,7 @@
 /*   By: daviles- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 17:50:25 by daviles-          #+#    #+#             */
-/*   Updated: 2023/05/31 22:21:57 by daviles-         ###   ########.fr       */
+/*   Updated: 2023/06/02 15:25:52 by daviles-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_fractol.h"
@@ -54,6 +54,24 @@ void	img_pix_put(t_img *img, int x, int y, int color)
 	*(int *)pixel = color;
 }
 
+int	isMandelbrot(int n, double z_re, double z_im, double c_im, double c_re)
+{
+	double Z_re2; 
+    double Z_im2;
+
+	while (n++ < MAX_ITERATION)
+	{
+		Z_re2 = Z_re * Z_re;
+		Z_im2 = Z_im * Z_im;
+		if(Z_re2 + Z_im2 > 4)
+			return (0);
+		Z_im = 2*Z_re*Z_im + c_im;
+		Z_re = Z_re2 - Z_im2 + c_re;
+	}
+	return (1); 
+}
+
+
 int	generate_fractal(t_data data)
 {
 	double MinRe = -2.0;
@@ -62,7 +80,6 @@ int	generate_fractal(t_data data)
 	double MaxIm = MinIm+(MaxRe-MinRe)*HEIGHT/WIDTH;
 	double Re_factor = (MaxRe-MinRe)/(WIDTH-1);
 	double Im_factor = (MaxIm-MinIm)/(HEIGHT-1);
-	unsigned MaxIterations = 30;
 	unsigned y;
 	unsigned x;
 	unsigned n;
@@ -78,11 +95,12 @@ int	generate_fractal(t_data data)
         double Z_re = c_re, Z_im = c_im;
         int isInside = 1;
 		n = 0;
-        while (n++ < MaxIterations)
+        while (n++ < MAX_ITERATION)
         {
             double Z_re2 = Z_re*Z_re, Z_im2 = Z_im*Z_im;
             if(Z_re2 + Z_im2 > 4)
             {
+				printf("ZRE %f ZRE2 %f  \n", Z_re, Z_re2);
                 isInside = 0;
                 break;
             }
@@ -96,73 +114,6 @@ int	generate_fractal(t_data data)
 	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img.mlx_img, 0, 0);
 	return (0);
 }
-
-int	generate_fractal2(t_data data)
-{
-	double	minRe;
-	double	maxRe;
-	double	minIm;
-	double	maxIm;
-	double	reFact;
-	double	imFact;
-	double	c_re;
-	double	c_im;
-	double	z_re;
-	double	z_im;
-	double	z_re2;
-	double	z_im2;
-	int		x;
-	int		y;
-	int		maxIter;
-	int		iter;
-	int		isSet;
-
-	maxIter = 100;
-	y = 0;
-	minRe = -2.0;
-	maxRe = 1.0;
-	minIm = -1.2;
-	maxIm = minIm + (maxRe - minRe) * (HEIGHT / WIDTH);
-	reFact = (maxRe - minRe) / (WIDTH - 1);
-	imFact = (maxIm - minIm) / (HEIGHT - 1);
-	while (y < HEIGHT)
-	{
-		x = 0;
-		c_im = maxIm - y * imFact;
-		while (x < WIDTH)
-		{
-			c_re = maxRe + (x * reFact);
-			z_re = c_re;
-			z_im = c_im;
-			iter = 0;
-			isSet = 1;
-			while (iter < maxIter)
-			{
-				z_re2 = z_re * z_re;
-				z_im2 = z_im * z_im;
-				if (z_re2 + z_im2 > 4)
-				{
-					isSet = 0;
-					break;
-				}
-				z_im = 2 * (z_re * z_im) + c_im;
-				z_re = z_re2 - z_im2 + c_re;
-				iter++;
-			}
-			if (isSet)
-			{
-				img_pix_put(&data.img, x, y, 0xFF0000); 
-			}
-			else
-				img_pix_put(&data.img, x, y, 0x00FF00); 
-			x++;
-		}
-		y++;
-	}
-	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img.mlx_img, 0, 0);
-	return (0);
-}
-
 
 int	main(void)
 {
@@ -187,12 +138,11 @@ int	main(void)
 	}
 	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, \
 							&data.img.ln_len, &data.img.endian);
-/* Setup hooks */ 
-//	mlx_loop_hook(data.mlx_ptr, &generate_fractal, &data);
+	/* Generate fractal*/
 	generate_fractal(data);
+	/* Setup hooks */ 
 	mlx_key_hook(data.win_ptr, &handle_keys, &data);
 	mlx_loop(data.mlx_ptr);
-
 	/* we will exit the loop if there's no window left, and execute this code */
 	free(data.mlx_ptr);
 
