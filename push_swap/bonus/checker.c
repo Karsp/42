dfb5712f91f8b985214b54b6a2bd59ac3afa08b1
David Aviles Leiva <daviles-@c1r16s1.42madrid.com>
2023-09-29 16:13:20 +0200

    Make new functions without print for bonus. Tested and clean

--- push_swap/bonus/checker.c
+++ push_swap/bonus/checker.c
@@ -6,7 +6,7 @@
 /*   By: daviles- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 01:09:54 by daviles-          #+#    #+#             */
-/*   Updated: 2023/09/29 02:33:38 by daviles-         ###   ########.fr       */
+/*   Updated: 2023/09/29 16:02:39 by daviles-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include"../srcs/push_swap.h"
@@ -14,27 +14,27 @@
 void	do_checker_moves(t_stack **stack_a, t_stack **stack_b, char *line, int len)
 {
 	if (!ft_strncmp(line, "ra", len - 1))
-		ft_rx(stack_a, 'a');
+		ft_b_rx(stack_a, 'a');
 	else if (!ft_strncmp(line, "rb", len - 1))
-		ft_rx(stack_b, 'b');
+		ft_b_rx(stack_b, 'b');
 	else if (!ft_strncmp(line, "rr", len - 1))
-		ft_rr(stack_a, stack_b);
+		ft_b_rr(stack_a, stack_b);
 	else if (!ft_strncmp(line, "rrb", len - 1))
-		ft_rrx(stack_b, 'b');
+		ft_b_rrx(stack_b, 'b');
 	else if (!ft_strncmp(line, "rra", len - 1))
-		ft_rrx(stack_a, 'a');
+		ft_b_rrx(stack_a, 'a');
 	else if (!ft_strncmp(line, "rrr", len - 1))
-		ft_rrr(stack_a, stack_b);
+		ft_b_rrr(stack_a, stack_b);
 	else if (!ft_strncmp(line, "pa", len - 1))
-		ft_pusha(stack_a, stack_b);
+		ft_b_pusha(stack_a, stack_b);
 	else if (!ft_strncmp(line, "pb", len - 1))
-		ft_pushb(stack_a, stack_b);
+		ft_b_pushb(stack_a, stack_b);
 	else if (!ft_strncmp(line, "ss", len - 1))
-		ft_swapab(stack_a, stack_b);
+		ft_b_swapab(stack_a, stack_b);
 	else if (!ft_strncmp(line, "sa", len - 1))
-		ft_swap(stack_a, 'a');
+		ft_b_swap(stack_a, 'a');
 	else if (!ft_strncmp(line, "sb", len - 1))
-		ft_swap(stack_b, 'b');
+		ft_b_swap(stack_b, 'b');
 	else
 		ft_printerror("\n");
 }
