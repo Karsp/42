7420d5f3d46818e728ab9d597e302479163d5bf6
David Aviles Leiva <daviles-@c1r11s1.42madrid.com>
2023-10-19 12:03:02 +0200

    Pipex base working, full tested.

--- pipex/srcs/main.c
+++ pipex/srcs/main.c
@@ -6,24 +6,19 @@
 /*   By: daviles- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 21:08:33 by daviles-          #+#    #+#             */
-/*   Updated: 2023/10/13 02:58:29 by daviles-         ###   ########.fr       */
+/*   Updated: 2023/10/19 11:56:25 by daviles-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../include/pipex.h"
 
-void	leaker(void)
-{
-	system("leaks -q pipex");
-}
-
 int	main(int ac, char **av, char **env)
 {
-	atexit(leaker);
 	if (ac == 5)
 	{
 		ft_pipex(av, env);
 	}
 	else
-		ft_printf("./pipex archivo1 comando1 comando2 archivo2\n");
+		ft_printf("Invalid args. Follow this example:\n \
+	./pipex archivo1 comando1 comando2 archivo2\n");
 	return (0);
 }
--- pipex/srcs/utils.c
+++ pipex/srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: daviles- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 02:05:47 by daviles-          #+#    #+#             */
-/*   Updated: 2023/10/13 02:28:07 by daviles-         ###   ########.fr       */
+/*   Updated: 2023/10/19 11:58:55 by daviles-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../include/pipex.h"
@@ -19,11 +19,7 @@ void	ft_waitpid(pid_t out_pid)
 	(void)out_pid;
 	pid = 1;
 	while (pid != -1)
-	{
 		pid = waitpid(-1, &status, 0);
-//		if (pid == out_pid)
-//			exit(0);
-	}
 }
 
 void	ft_perror_exit(char *msj)
